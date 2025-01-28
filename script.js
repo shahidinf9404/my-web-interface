64e3be00f996fe6e0e22c8442e606dc878bfcf53
shahidinf9404 <126430297+shahidinf9404@users.noreply.github.com>
2025-01-28 17:13:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Senarai fail PDF yang terdapat dalam repositori GitHub (gantikan dengan URL sebenar)
 const pdfDocuments = [
-    { name: 'BORANG TEMPAHAN KENDERAAN BDHA 2024 (Hanizam)', url: 'https://github.com/username/my-repository/raw/main/BORANG%20TEMPAHAN%20KENDERAAN%20BDHA%202024%20(Hanizam).pdf' },
-    { name: 'Panduan Pengguna Perkhidmatan PCN Wifi', url: 'https://github.com/username/my-repository/raw/main/Panduan%20Pengguna%20Perkhidmatan%20PCN%20Wifi.pdf' },
-    { name: 'COUNTRIES THAT REQUIRE VISA & DO NOT REQUIRE VISA', url: 'https://github.com/username/my-repository/raw/main/COUNTRIES_THAT_REQUIRE%20VISA&DO_NOT_REQUIRE_VISA.pdf' }
+    { name: 'BORANG TEMPAHAN KENDERAAN BDHA 2024 (Hanizam)', url: 'https://github.com/shahidinf9404/my-web-interface/raw/main/BORANG%20TEMPAHAN%20KENDERAAN%20BDHA%202024%20(Hanizam).pdf' },
+    { name: 'Panduan Pengguna Perkhidmatan PCN Wifi', url: 'https://github.com/shahidinf9404/my-web-interface/raw/main/Panduan%20Pengguna%20Perkhidmatan%20PCN%20Wifi.pdf' },
+    { name: 'COUNTRIES THAT REQUIRE VISA & DO NOT REQUIRE VISA', url: 'https://github.com/shahidinf9404/my-web-interface/raw/main/COUNTRIES_THAT_REQUIRE%20VISA&DO_NOT_REQUIRE_VISA.pdf' }
 ];
 
 // Fungsi untuk mencari dokumen PDF
