cf6572ef8e7aaf35d7561b81afda1e20400140ae
Shahidin Ashraf <shahidin.ashraf@moh.gov.my>
2025-01-29 15:48:18 +0800

    add web files and PDFs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const pdfs = [
     { title: "BORANG TEMPAHAN KENDERAAN BDHA 2024(Hanizam)", file: "pdfs/BORANG%20TEMPAHAN%20KENDERAAN%20BDHA%202024%28Hanizam%29.pdf" },
     { title: "Panduan Pengguna Perkhidmatan PCN Wifi", file: "pdfs/Panduan%20Pengguna%20Perkhidmatan%20PCN%20Wifi.pdf" },
-    { title: "COUNTRIES_THAT_REQUIRE VISA&DO_NOT_REQUIRE_VISA", file: "pdfs/COUNTRIES_THAT_REQUIRE%20VISA%26DO_NOT_REQUIRE%20VISA.pdf" }
+    { title: "COUNTRIES_THAT_REQUIRE_VISA_and_DO_NOT_REQUIRE_VISA", file: "pdfs/COUNTRIES_THAT_REQUIRE_VISA_and_DO_NOT_REQUIRE_VISA.pdf" }
 ];
 
 let selectedPDF = '';
